138e03a383b033ff4afd8120b1c263f60011f97c
AdrianDali <hadriandali@gmail.com>
2022-06-07 18:25:43 -0500

    git ignore

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import  '../styles/NotFound.scss'
+const NotFound = () => {
+	return (
+		<div>
+			No encontrado, error 404.
+		</div>
+	);
+}
+
+export default NotFound;
