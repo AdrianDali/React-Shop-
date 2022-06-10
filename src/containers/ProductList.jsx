c8a676e8093fed572e6f41d513c5731369fe91c8
AdrianDali <hadriandali@gmail.com>
2022-06-10 14:13:17 -0500

    Hooks

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect}from 'react';
 import ProductItem from '../components/ProductItem';
 import '../styles/ProductList.scss';
+import axios from 'axios';
+
+const API = 'https://api.escuelajs.co/api/v1/products'
 
 const ProductList = () => {
+	const [products, setProducts] = useState([]);
+	/**useEffect recibe una funcion anonima y un arreglo */
+	useEffect(async() => {
+		const response = await axios(API);
+		setProducts(response.data);
+	}, [])
+
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => {
+					return <ProductItem />
+				})}
+				
 			</div>
 		</section>
 	);
